557a43ce39fabcf3fb63c9894359e8414bf1b1bc
Cristian Dreyer <cristianbr.dreyer@gmail.com>
2022-11-21 15:47:31 -0300

    goals services done

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -2,10 +2,12 @@ import { Router, Request, Response } from "express";
 import { prismaClient } from "./prismaClient";
 import { verifySession, createSession } from "./Auth/session";
 import { UserServices } from "./services/UserServices"
+import { GoalServices } from "./services/GoalServices"
 
 export const router = Router();
 
-const userService = new UserServices();
+const userServices = new UserServices();
+const goalServices = new GoalServices();
 
 // ----- HOME -----
 
@@ -24,7 +26,7 @@ router.get("/login", async (req: Request, res: Response) => {
       return res.json("need fill all fields")
    }
 
-   const user = await userService.authData(email, password)
+   const user = await userServices.authData(email, password)
    if (!user) {
       return res.json("incorrect email or password");
    }
@@ -38,7 +40,7 @@ router.get("/login", async (req: Request, res: Response) => {
 router.get("/user/infos", verifySession, async (req: Request, res: Response) => {
 
    const id: number = +req.cookies.id;
-   const user = await userService.getData(id);
+   const user = await userServices.getData(id);
    if (!user) {
       return res.json("account not found")
    }
@@ -57,7 +59,7 @@ router.get("/users", async (req: Request, res: Response) => {
 router.post("/user", async (req: Request, res: Response) => {
 
    const { name, password, email } = req.body;
-   const new_user = await userService.insertData({ name, password, email })
+   const new_user = await userServices.insertData({ name, password, email })
 
    return res.json(new_user);
 })
@@ -66,23 +68,25 @@ router.post("/user", async (req: Request, res: Response) => {
 // ----- GOAL -----
 
 // create goal
-router.post("/goal", async (req: Request, res: Response) => {
-   const { title, description, value, achievement_time, user_id } = req.body;
+router.post("/goal", verifySession, async (req: Request, res: Response) => {
+
+   const { title, description, value, achievement_time } = req.body;
+   const user_id: number = +req.cookies.id
+
+   if (user_id === undefined || title == "" || description == "") return res.json("fill al main fields")
 
    const { year, month, day } = achievement_time;
    const date = new Date(year, month, day);
 
-   if (user_id === undefined || title == "" || description == "") return res.json("fill al main fields")
+   const new_goal = await goalServices.insertData({ title, description, value, achievement_time: date, user_id })
 
-   const new_goal = await prismaClient.goal.create({
-      data: {
-         title: title,
-         description: description,
-         value: value,
-         achievement_time: date,
-         user_id: user_id
-      }
-   })
 
    return res.json(new_goal);
+})
+
+router.get("/goals", verifySession, async (req: Request, res: Response) => {
+   const user_id: number = +req.cookies.id;
+   const all_goals = await goalServices.getGoals(user_id)
+
+   return res.json(all_goals);
 })
--- backend/src/services/GoalServices.ts
+++ backend/src/services/GoalServices.ts
@@ -1,5 +1,38 @@
-import {prismaClient} from "../prismaClient"
+import { prismaClient } from "../prismaClient"
 
-export class GoalServices{
+interface GoalRequest {
+   title: string,
+   description: string,
+   value: number,
+   achievement_time: Date,
+   user_id: number
+}
 
+
+export class GoalServices {
+
+   async insertData({ title, description, value, achievement_time, user_id }: GoalRequest) {
+
+      const new_goal = await prismaClient.goal.create({
+         data: {
+            title: title,
+            description: description,
+            value: value,
+            achievement_time: achievement_time,
+            user_id: user_id
+         }
+      })
+
+      return new_goal;
+   }
+
+   async getGoals(user_id: number){
+      const goals = await prismaClient.goal.findMany({
+         where:{
+            user_id: user_id
+         }
+      })
+
+      return goals;
+   }
 }
