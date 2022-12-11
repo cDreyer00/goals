835fde57cc562df2754f458811e1ac8c86726ab9
Cristian Dreyer <cristianbr.dreyer@gmail.com>
2022-12-11 11:12:01 -0300

    oracle database config done

--- temp-backend/src/handlers/user_handlers.js
+++ temp-backend/src/handlers/user_handlers.js
@@ -1,4 +1,4 @@
-import {loginUserService} from "../services/user_services.js"
+import {loginUserService, getAllUsersService, createUserService} from "../services/user_services.js"
 
 export async function loginUserHandler(req, res) {
     const {email, password} = req.body;
@@ -6,12 +6,18 @@ export async function loginUserHandler(req, res) {
     if(!email || !password){
         return res.status(400).send("need fill all fields");
     }
-
     const users = await loginUserService({email, password});
-
+    users.rows.test = "aaa";
+    console.log(users.rows.test)
     return res.json(users);
 }
 
 export async function createUserHandler(req, res){
+    const {name, email, password} = req.body;
 
+    if(!name || !email || !password){
+        return res.status(400).send("nedd fill all fields");
+    }
+
+    const result = await createUserService({name, email, password});
 }
