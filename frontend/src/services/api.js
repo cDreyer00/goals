39224c27b7316383dc615823685cdc6631117898
Cristian Dreyer <cristianbr.dreyer@gmail.com>
2022-12-29 18:49:02 -0300

    fixing delete bug

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,13 +2,13 @@
 
 import axios from "axios";
 
-// const api = axios.create({
-//     baseURL: "https://backend-cdreyer00.vercel.app/"
-// })
-
-
 const api = axios.create({
-    baseURL: "http://localhost:3001/"
+    baseURL: "https://backend-cdreyer00.vercel.app/"
 })
+
+
+// const api = axios.create({
+//     baseURL: "http://localhost:3001/"
+// })
 
 export default api;
